dee323d081f71b648748a0fc2b1b16c462760901
khadija-codencoit <khadija.khatun@codencoit.com>
2025-09-02 23:08:04 +0600

    String Methos

---
+++ PromitiveType/string_methos.py
@@ -0,0 +1,6 @@
+course = "python programming"
+print(course.upper())
+print(course.title()) #Here course is a object and object has method like upper,title
+print(course.find("py"))
+print("pro" in course) #This is expression.An expression is any piece of code that produces a value when it is run.
+print("swif" not in course)
---
+++ app.py
@@ -0,0 +1,4 @@
+y = 1
+x = 2
+kk = 8
+course_name = "python"

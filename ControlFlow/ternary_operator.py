7a07c86bf16a1d28aee98e0a012517282b35282b
khadija-codencoit <khadija.khatun@codencoit.com>
2025-09-03 22:04:09 +0600

    Username and Password Checker Using Ternary Operator

--- ControlFlow/ternary_operator.py
+++ ControlFlow/ternary_operator.py
@@ -19,4 +19,18 @@ user_age = int(input("Enter your age : "))
 text = "Eligible" if user_age >= 18 else "Not Eligible"
 print(text)
 
+## 3. Login System
+# Input: Username and password
+#Check if they match stored values: "admin", "1234"
+# Output: "Login successful" or "Access denied"
+
+correct_user = "admin"
+correct_password = 12345
+
+username = input("Enter username : ")
+password = int(input("Enter your password : "))
+
+message = "Login successful" if username == correct_user and password == correct_password else "Access denied"
+print(message)
+
 

c9d681a6e5b04591b55758cdbb3e78a3b665ab94
khadija-codencoit <khadija.khatun@codencoit.com>
2025-09-18 23:09:49 +0600

    Add examples of list creation and operations

---
+++ Data Structures/list.py
@@ -0,0 +1,23 @@
+# A list of letters
+letter = ["a", "b", "c", "d"]
+
+# A 2D list (list of lists) – behaves like a simple matrix
+matrix = [[1, 2], [3, 4]]
+
+# Creates a list with five zeros
+zeros = [0] * 5
+print(zeros)
+
+# Concatenates the zeros list with the matrix
+combined = zeros + matrix
+print(combined)
+
+# Creates a list of numbers from 0 to 19
+num = list(range(20))
+print(num)
+
+# Converts a string into a list of characters (including spaces)
+chars = list("Hello World")
+print(chars)
+
+

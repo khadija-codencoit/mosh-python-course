0f96475d84e751a0bf5fd7944494249297cbf508
khadija-codencoit <khadija.khatun@codencoit.com>
2025-09-21 20:44:54 +0600

    Employee Sorting List

--- Data Structures/sorting_list.py
+++ Data Structures/sorting_list.py
@@ -14,3 +14,20 @@ def sort_item(item):
 
 items.sort(key = sort_item)
 print(items)
+
+# Problem - 1
+# You have employee data with name and salary:
+# Sort employees based on salary.
+# Print the highest-paid employee.
+
+Employee = [
+    ("Tamim",20000),
+    ("Sakib",50000),
+    ("Masrafi",40000)
+]
+
+def sort_item(item):
+    return item[1]
+
+Employee.sort(key=sort_item)
+print(Employee)

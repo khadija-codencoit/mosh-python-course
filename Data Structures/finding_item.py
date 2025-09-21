5cad8700099f9b338366299587e4ff81a40880c9
khadija-codencoit <khadija.khatun@codencoit.com>
2025-09-21 17:23:56 +0600

    Finding Item

---
+++ Data Structures/finding_item.py
@@ -0,0 +1,14 @@
+# Problem - 1
+
+letters = ["a","c","d"]
+letters.count("d")
+print(letters)
+
+if "d" in letters:
+    print(letters.index("d")) 
+
+# Problem -2
+fruits = ["apple", "banana", "mango", "banana", "orange"]
+
+if "banana" in fruits:
+    print(fruits.index("banana"))

15bd30b9f2b5b68b15b31bfc38f3cce2888e7021
khadija-codencoit <khadija.khatun@codencoit.com>
2025-09-22 17:21:05 +0600

    Add browsing session stack example

---
+++ Data Structures/stack.py
@@ -0,0 +1,9 @@
+browsing_session = []
+browsing_session.append(1)
+browsing_session.append(23)
+browsing_session.append(88)
+print(browsing_session)
+last = browsing_session.pop()
+print(browsing_session)
+
+print("redirect", browsing_session[-1])

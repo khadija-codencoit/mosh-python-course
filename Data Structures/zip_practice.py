af087cc538a75fa5d225bf49f89e58a79a70f6e8
khadija-codencoit <khadija.khatun@codencoit.com>
2025-09-22 16:32:02 +0600

    Combine products with prices using zip

--- Data Structures/zip_practice.py
+++ Data Structures/zip_practice.py
@@ -3,3 +3,12 @@ list2 = [9,6,7,7]
 
 combine = list(zip(list1,list2))
 print(combine)
+
+#Problem - 1
+#You are managing an online store. You have two lists
+
+products = ["T-shirt", "Jeans", "Sneakers", "Hat"]
+prices = [2000,800,20,100]
+
+combine = list(zip(products,prices))
+print(combine)

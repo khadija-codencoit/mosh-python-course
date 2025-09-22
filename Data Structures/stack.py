deac92e22d0a79e1a74de76e72f4e74f5820535c
khadija-codencoit <khadija.khatun@codencoit.com>
2025-09-22 18:14:43 +0600

    Undo Feature in a Text Editor

--- Data Structures/stack.py
+++ Data Structures/stack.py
@@ -7,3 +7,21 @@ last = browsing_session.pop()
 print(browsing_session)
 
 print("redirect", browsing_session[-1])
+
+#problem - 1
+#Problem: Undo Feature in a Text Editor
+
+Text_Editor = []
+
+def type_word(word):
+    Text_Editor.append(word)
+    print("Current Text: ", " ".join(Text_Editor))
+
+def undo():
+    removed = Text_Editor.pop()
+    print(f"Undo last word ({removed}) → Current text:", " ".join(Text_Editor))
+
+
+
+type_word("Hello")
+undo()

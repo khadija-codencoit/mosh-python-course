84d120adffe8d6eb5072b76e522928bcc2453a6f
khadija-codencoit <khadija.khatun@codencoit.com>
2025-09-24 23:56:56 +0600

    Making Container

---
+++ Classes/making_container.py
@@ -0,0 +1,15 @@
+class TagCloud:
+
+    def __init__(self):
+        self.tags = {}
+
+    def add(self,tag):
+        self.tags[tag] = self.tags.get(tag,0) + 1
+
+     
+cloud = TagCloud()
+cloud.add("python")
+cloud.add("python")
+cloud.add("Python")
+
+print(cloud.tags)

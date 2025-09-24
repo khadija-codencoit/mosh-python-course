17dce6deeea4a884fc60582b5bcddce2c27c9c64
khadija-codencoit <khadija.khatun@codencoit.com>
2025-09-24 19:15:24 +0600

    Class vs Instance Attribute

--- Classes/class_vs_instance_Attribute.py
+++ Classes/class_vs_instance_Attribute.py
@@ -1,11 +1,13 @@
 class Point:
+    defalut_colour = "red"
+
     def __init__(self,x,y):
         self.x = x
         self.y = y
 
-    def drew(self):
+    def draw(self):
         print(f"Point {self.x} and {self.y}")
 
-point = Point(1,22)
-
-point.drew()
+Point.defalut_colour = "yellow"
+point = Point(1,3)
+point.draw()

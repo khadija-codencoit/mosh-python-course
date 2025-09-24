4807d17a84ee085b417448c5d7525f32581bc891
khadija-codencoit <khadija.khatun@codencoit.com>
2025-09-24 22:04:43 +0600

    Comparing Objects

--- Classes/magic_method.py
+++ Classes/magic_method.py
@@ -1,3 +1,27 @@
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return f"({self.x}, {self.y})"
+
+    def draw(self):
+        print(f"Point ({self.x}, {self.y})")
+
+point = Point(1, 2)
+print(str(point))
+
+
+
+
+
+
+
+
+
+
+
 class Person:
     def __init__(self, name, age):
         self.name = name

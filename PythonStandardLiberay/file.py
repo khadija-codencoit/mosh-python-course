c88ae825d32c1564bd79739884dc8c81a7456a96
khadija-codencoit <khadija.khatun@codencoit.com>
2025-09-26 02:24:44 +0600

    File and Shutil

---
+++ PythonStandardLiberay/file.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+from time import ctime
+import shutil
+import datetime
+
+data = Path("Classes/constractor.py")
+source = Path("sample.py")
+
+
+print(shutil.copy(data,source))
+
+shutil.copyfile(data, source, follow_symlinks=True)
+shutil.copymode(data, source, follow_symlinks=True)
+
+
+# print(data.read_text())
+# source.write_text(data.read_text())
+
+# print(source.rename("kk"))
+# source.rename("sample.py")
+# print(source.exists())
+
+
+# print(source.stat().st_ctime)
+# print(ctime(source.stat().st_mtime))
+# print(datetime.datetime.now())
+
+
+# source.write_text()
+print(source.read_text())
---
+++ sample.py
@@ -0,0 +1,11 @@
+class Point:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+
+    def drew(self):
+        print(f"Point {self.x} and {self.y}")
+
+point = Point(1,22)
+
+point.drew()

519f061c66d813fd28daa3e56656b0a3042f2439
khadija-codencoit <khadija.khatun@codencoit.com>
2025-09-26 02:05:02 +0600

    Path and Directory

---
+++ PythonStandardLiberay/path_directory.py
@@ -0,0 +1,19 @@
+
+from pathlib import Path
+
+path = Path("Data Structures")
+
+print("This is for exists : ",path.exists())
+print("This is for file directory : ",path.is_file())
+
+print(path.name)
+print(path.stem)
+
+array = []
+
+for i in path.iterdir():
+    print(i)
+    if i.is_dir():
+        array.append(i)
+
+print(array)

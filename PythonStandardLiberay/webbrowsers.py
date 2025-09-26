e03af13847e7a504a505ac7e15c88b1254084c0e
khadija-codencoit <khadija.khatun@codencoit.com>
2025-09-26 19:35:49 +0600

    Implement user input for selecting social media

--- PythonStandardLiberay/webbrowsers.py
+++ PythonStandardLiberay/webbrowsers.py
@@ -3,3 +3,18 @@ import time
 
 print("welcome")
 webbrowser.open("http://facebook.com")
+
+
+
+#Problem-1
+socile_median = ['facebook','youtube']
+
+for index  , i in enumerate(socile_median,start=1):
+    print(f"{index} {i}")
+
+choice = input("Give your choice 1,2 : ")
+
+if choice == 1:
+    webbrowser.open("http://facebook.com")
+else:
+    webbrowser.open("http://youtube.com")

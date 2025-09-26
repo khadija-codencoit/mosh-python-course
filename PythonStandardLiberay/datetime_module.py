3ba0db5ce12716da296d09682271ee152e021dcb
khadija-codencoit <khadija.khatun@codencoit.com>
2025-09-26 16:01:29 +0600

    DateTimes

---
+++ PythonStandardLiberay/datetime_module.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+import time
+
+dt = datetime(2018,1,1)
+dt2 = datetime.now
+dt = datetime.strptime("2018/01/01","%Y/%d/%m")
+dt = datetime.fromtimestamp(time.time())
+
+print(f"{dt.year}/{dt.month}")
+print(dt.strftime("%Y/%m"))
